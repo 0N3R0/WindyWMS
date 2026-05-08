abda58104f936a8da613a4d6d67e255a1123dc55
0N3R0 <dawid175571@gmail.com>
2026-05-08 02:52:31 +0200

    feat(frontend): delete unused files and update constants

--- frontend/src/shared/types/constants.types.ts
+++ frontend/src/shared/types/constants.types.ts
@@ -1,11 +1,47 @@
 export interface AppConstants {
   auth: {
     jwtExpirationSeconds: number;
+    passwordMinLength: number;
+    messages: {
+      errors: {
+        EMAIL_TAKEN: string;
+        INVALID_CREDENTIALS: string;
+        USER_NOT_FOUND: string;
+        MISSING_TOKEN: string;
+        INVALID_TOKEN: string;
+      };
+    };
   };
   shipments: {
-    cronSimulationIntervalSeconds: number;
     minWeight: number;
     maxWeight: number;
     availableStatuses: string[];
+    sorting: {
+      allowedFields: string[];
+      defaultField: string;
+      availableOrders: string[];
+      defaultOrder: string;
+    };
+    config: {
+      trackingNumberPrefix: string;
+      cronSimulationIntervalSeconds: number;
+    };
+    messages: {
+      events: {
+        PENDING: string;
+        CANCELLED: string;
+        IN_TRANSIT: string;
+        DELIVERED: string;
+      };
+      errors: {
+        NOT_FOUND: string;
+        CANNOT_CANCEL: string;
+      };
+    };
+  };
+  pagination: {
+    defaultPage: number;
+    defaultLimit: number;
+    maxLimit: number;
   };
 }
